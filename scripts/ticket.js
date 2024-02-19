a77857a2672b11102a90fca518636c6e261a80b7
Nafis185 <nafisfuad185@gmail.com>
2024-02-19 23:32:00 +0600

    ticket selction

---
+++ scripts/ticket.js
@@ -0,0 +1,24 @@
+
+// function ticketClick() {
+//   const element = document.getElementById("ticketSection");
+//   if (element.style.display === "none") {
+//     element.style.display = "Block";
+//   } else {
+//     element.style.display = "none";
+//   }
+// }
+
+  
+
+
+
+
+function ticket () {
+  hideElementById('ticketSection');
+
+  showElementById('ticketSuccess');
+}
+
+
+
+
